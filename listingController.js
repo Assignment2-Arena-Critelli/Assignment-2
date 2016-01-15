cad1dde45446846ac3f2362b6f803f314bd98051
mxiia <mxiia@mxiia.com>
2016-01-15 12:04:18 -0500

    final

---
+++ listingController.js
@@ -0,0 +1,25 @@
+angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
+  function($scope, Listings) {
+    $scope.listings = Listings;
+    $scope.detailedInfo = undefined;
+	$scope.newListing = {};
+	$scope.detailInfo = {};
+
+    /* 
+      Implement these functions in the controller to make your application function 
+      as described in the assignment spec. 
+     */
+    $scope.addListing = function() {
+		$scope.listings.push($scope.newListing);
+		$scope.newListing = {};
+	};
+    $scope.deleteListing = function(listing) {
+		var index = $scope.listings.indexOf(listing);
+		$scope.listings.splice(index,1);
+	};
+    $scope.showDetails = function(listing) {
+		var index = $scope.listings.indexOf(listing);
+		$scope.detailInfo = $scope.listings[index];
+	};
+  }
+]);
